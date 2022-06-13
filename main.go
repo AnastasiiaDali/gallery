04d4f02c4cd8d7005db37e20186cc525e5747899
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-13 21:56:22 +0100

    refactor according to the new version of the course

--- main.go
+++ main.go
@@ -2,37 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
-	"gallery/views"
 	"github.com/go-chi/chi/v5"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.Execute(w, nil)
+func ExecuteTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		panic(err)
+		log.Printf("parsing template: %v", filepath)
+		http.Error(w, "there was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", filepath)
+		http.Error(w, "there was an error executing the template", http.StatusInternalServerError)
+		return
 	}
 }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tplpath := filepath.Join("templates", "home.gohtml")
+	ExecuteTemplate(w, tplpath)
+}
 
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.Execute(w, nil)
-	if err != nil {
-		panic(err)
-	}
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	ExecuteTemplate(w, tplPath)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	ExecuteTemplate(w, tplPath)
 }
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", home)
-	r.Get("/contact", contact)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
