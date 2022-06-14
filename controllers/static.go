d05df74d94ad758fb2ab8818f89dd88bce97710a
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-06-14 17:07:27 +0100

    make FAQ page functionality to dinamically provide questions

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"gallery/views"
@@ -9,5 +10,29 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "How are you doing?",
+			Answer:   "I am alright",
+		},
+		{
+			Question: "How are you doing?",
+			Answer:   "I am alright",
+		},
+		{
+			Question: "How are you doing?",
+			Answer:   "I am alright",
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
